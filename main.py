25c835decd23212dfbb70fd0332a6badf69407c9
atharvapawar9 <atharvapawar003@gmail.com>
2025-07-25 16:36:36 +0530

    Added new endpoints to main.py for retrieving patient data and sorting patients by height, weight, or bmi. Updated import statement to include additional FastAPI modules.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -22,3 +22,28 @@ def view():
     data = load_data()
 
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description= 'ID of the patient to DB', example= 'P001')):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail='Patient not found')
+
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description="Sort by height, weight, or bmi", example="height"), order: str = Query('asc', description="Sort order", example="asc")):
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid sort field selected. Please select from {valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail='Invalid sort order selected. Please select from asc or desc')
+    
+    data = load_data()
+
+    sort_order = True if order=='desc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by,0), reverse=sort_order)
+
+    return sorted_data
